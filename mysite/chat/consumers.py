fea2f19c240456c8f9aa48d17e6b90e16f443b57
kotnid <tkt0506tkt@gmail.com>
2021-07-08 20:04:37 +0800

    last update

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -1,17 +1,17 @@
-import json 
+import json
 from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-    def disconnect(self,close_code):
+    def disconnect(self, close_code):
         pass
 
-    def receive(self,text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        print("msg recv")
+
         self.send(text_data=json.dumps({
-            'message':message
-        }))    
+            'message': message
+        }))
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # this is just copy code from https://channels.readthedocs.io/en/stable/tutorial/part_1.html
 
-## error : msg not show
+## error : msg not show4
